230a646e97cc2165895141123992b95ee13f3fff
ArtemFrun <tjomka777@gmail.com>
2020-07-04 18:23:30 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(window).load(function() { 
+    $('.slider').slick({
+        arrows: false,
+        vertical: true,
+        verticalSwiping: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnFocus:false,
+        pauseOnHover: false,
+        pauseOnDotsHover:false,
+        graggable: false,
+        swipe: false
+    });
+
     $("a[href^='#']").click(function(){
         var _href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(_href).offset().top});
