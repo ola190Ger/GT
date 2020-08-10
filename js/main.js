19d622d37ec956838dca167af213d1c8d72fc5a1
ArtemFrun <tjomka777@gmail.com>
2020-08-10 22:15:03 +0300

    pop-up window

--- js/main.js
+++ js/main.js
@@ -19,3 +19,45 @@ $(window).load(function() {
         return false;
     });
 });
+
+$(document).ready(function(){
+    $(".openinfo_1").click(function(){
+    $(".services-block__info-1").slideToggle("2000");
+    if($("div").hasClass("blockall"))
+    $(".blockall").remove();
+    else
+    $(".tytoknoall-1").append('<div class="blockall"></div>');
+    });
+    $(".tytoknoall-1").click(function(){
+    $(".services-block__info-1").slideToggle("2000");
+    $(".blockall").remove();
+    });   
+});
+
+$(document).ready(function(){
+    $(".openinfo_2").click(function(){
+        $(".services-block__info-2").slideToggle("2000");
+        if($("div").hasClass("blockall"))
+        $(".blockall").remove();
+        else
+        $(".tytoknoall-2").append('<div class="blockall"></div>');
+        });
+        $(".tytoknoall-2").click(function(){
+        $(".services-block__info-2").slideToggle("2000");
+        $(".blockall").remove();
+        });  
+});
+
+$(document).ready(function(){
+    $(".openinfo_3").click(function(){
+        $(".services-block__info-3").slideToggle("2000");
+        if($("div").hasClass("blockall"))
+        $(".blockall").remove();
+        else
+        $(".tytoknoall-3").append('<div class="blockall"></div>');
+        });
+        $(".tytoknoall-3").click(function(){
+        $(".services-block__info-3").slideToggle("2000");
+        $(".blockall").remove();
+        });  
+});
